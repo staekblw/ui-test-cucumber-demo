5f5822bd8a3d31ba9ef480c84a1d4abb1325abdb
lbwang <lbwang@thoughtworks.com>
2018-01-12 15:41:22 +0800

    simplify the code

---
+++ features/common/worlds.js
@@ -0,0 +1,42 @@
+const {setDefaultTimeout, setWorldConstructor} = require('cucumber');
+const guid = require("../common/guid")
+const driver = require("./driver");
+
+const IndexPage = require("../devices/pages/indexpage")
+
+const SECOND = 1000;
+setDefaultTimeout(60 * SECOND);
+
+class CustomWorld {
+    constructor({attach, parameters}) {
+        this._attach = attach;
+        this._parameters = parameters;
+        this._linbo = "linbo"
+        this._browser = driver.browser();
+        this._indexPage = new IndexPage(this._browser);
+    }
+
+    get attach() {
+        return this._attach;
+    }
+
+    get browser() {
+        return this._browser;
+    }
+
+    get indexPage() {
+        return this._indexPage;
+    }
+
+    dispose() {
+        this._browser.close();
+    }
+
+    async takeScreen() {
+        let screenFile = "reports/screenshots/" + guid() + '.png';
+        var png = await this.browser.getScreenshot(screenFile);
+        await this.attach(png, 'image/png');
+    }
+}
+
+setWorldConstructor(CustomWorld)
